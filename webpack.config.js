66824dee86b1ff7aabac7b32efae6957ad97cab0
Keno Leon <me@k3no.com>
2025-06-10 20:30:06 -0600

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/CrowCrow/',
+    publicPath: '/crowcrow/',
   },
   plugins: [
     new CopyPlugin({
